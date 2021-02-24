4985499f1ee0442ec6808466a6fdbee329dff21a
thinhle939 <thinhle939@gmail.com>
2021-02-24 21:36:56 +0700

    init project

--- app/models/word_answer.rb
+++
@@ -1,2 +0,0 @@
-class WordAnswer < ApplicationRecord
-end
--- test/models/lesson_word_test.rb
+++
@@ -1,7 +0,0 @@
-require "test_helper"
-
-class LessonWordTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
